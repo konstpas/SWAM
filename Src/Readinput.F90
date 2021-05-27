8edb22226ecd1d4d7c915bfd35a2d8bcb6e90bb7
Federico Lucco Castello <flc@kth.se>
2021-05-27 22:55:25 +0200

    Changed all source file type to .F90. This type is necessary because some files contain C-like commands such as #if (see energy_mod.F90 for an example)

---
+++ Src/Readinput.F90
@@ -0,0 +1,13 @@
+module mod_Readinput
+        
+        implicit none
+        
+contains 
+                
+         subroutine Readinput
+                        
+         	print *, 'Readinput test' 
+                        
+         end subroutine 
+       
+end module mod_Readinput
---
+++ Src/tagging_mod.F90
@@ -0,0 +1,58 @@
+module tagging_module
+
+  use iso_c_binding
+
+  use amrex_fort_module
+
+  implicit none
+
+  private
+
+  public :: tag_phi_error
+
+contains
+
+       
+  subroutine tag_phi_error (level, time, lo, hi, xlo, dx, surfdist, phi, philo, phihi, tag, taglo, taghi, phierr, &
+       settag, cleartag)  
+    use domain_module, only : get_surf_pos   
+    use material_properties_module, only : enth_at_melt  
+    integer               , intent(in   ) :: level, lo(3), hi(3), philo(4), phihi(4), taglo(4), taghi(4)
+    real(amrex_real)      , intent(in   ) :: phi(philo(1):phihi(1),philo(2):phihi(2),philo(3):phihi(3))
+    real(amrex_real)      , intent(in   ) :: xlo(3), dx(3)  ! Surface y-position (to be multifab on DIM-1) 
+    real(amrex_real)      , intent(in   ) :: surfdist ! distance from interface to refine grid
+    character(kind=c_char), intent(inout) :: tag(taglo(1):taghi(1),taglo(2):taghi(2),taglo(3):taghi(3))
+    real(amrex_real)      , intent(in   ) :: time, phierr
+    character(kind=c_char), intent(in   ) :: settag, cleartag
+    real(amrex_real)                      :: surfpos(lo(1):hi(1),lo(3):hi(3))  ! Array surface position 
+
+    integer :: i,j,k
+
+    real(amrex_real) :: phierr_this
+    real(amrex_real) :: ydist			! distance from free interface
+
+
+
+    call get_surf_pos(xlo, dx, lo, hi, surfpos)
+    do       k = lo(3), hi(3)
+       do    j = lo(2), hi(2)
+          do i = lo(1), hi(1)
+            
+             ydist = abs(xlo(2) + (j-lo(2))*dx(2) - surfpos(i,k) ) 
+             if (ydist .le. surfdist) then ! Free surface interface highly resolved 
+                tag(i,j,k) = settag
+             endif 
+             if (phi(i,j,k).ge.enth_at_melt) then ! all molten elements highly resolved 
+             	tag(i,j,k) = settag  
+             endif 
+             
+          enddo
+       enddo
+    enddo
+    
+    
+    
+  end subroutine tag_phi_error
+
+end module tagging_module
+
