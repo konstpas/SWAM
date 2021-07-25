cb8e0f46d32e524b6b72975c3d6f62cf05de44c9
Federico Lucco Castello <flc@kth.se>
2021-07-25 17:12:56 +0200

    Added extra comments to material_properties module

--- src/material_prop_mod.f90
+++ src/material_prop_mod.f90
@@ -142,7 +142,8 @@ contains
   
   ! ------------------------------------------------------------------
   ! Subroutine used to compute the tables that relate the enthalpy
-  ! and the temperature
+  ! and the temperature. Note that the temperature is in K and the
+  ! enthalpy in J/m^3
   ! ------------------------------------------------------------------ 
   subroutine init_mat_prop()
 
@@ -157,6 +158,10 @@ contains
     real(amrex_real) :: rhocp_i
     real(amrex_real) :: rhocp_im1
 
+    ! Allocate the temperature and enthalpy tables
+    allocate(temp_table(0:phiT_table_n_points))
+    allocate(enth_table(0:phiT_table_n_points))     
+    
     ! Initialize maximum diffusivity
     max_diffus = 0.
     
@@ -164,70 +169,70 @@ contains
     call get_m_A
     call get_melting_point
     
-    ! give end temperature point and data points in input file? 
-    ! now called several times, move call to initdata later 
-    allocate(temp_table(0:phiT_table_n_points))
-    allocate(enth_table(0:phiT_table_n_points))     
-    
-    ! Trapezoidal integration for enthalpy vs temperature, including phase transfer discontinuity  
-    ! Enthalpy is phi = int_0^T rho(T')*Cp(T') dT' 
-    ! Perhaps use larger number of data points for integration than for table. 
+    ! The enthalpy is phi = int_0^T rho(T')*Cp(T') dT' and trapezoidal integration is used
+    ! to compute the integral. The calculation of the enthalpy includes the phase transfer
+    ! discountinuity. 
 
     ! Table increment
-    phiT_table_dT = phiT_table_max_T/phiT_table_n_points  ! Equal increment to end temperature
+    phiT_table_dT = phiT_table_max_T/phiT_table_n_points
 
     ! Properties at zero temperature
-    temp_table(0) = 0_amrex_real  ! Temperature table 
+    temp_table(0) = 0_amrex_real
     call get_rho(temp_table(0),rho) 
     call get_Cp(temp_table(0),Cp)  
-    rhocp_i = rho*Cp ! product of density and heat capacity at temperature 
-    enth_table(0) = 0_amrex_real  ! Enthalpy table
+    rhocp_i = rho*Cp ! product of density and heat capacity 
+    enth_table(0) = 0_amrex_real
 
-    ! Fill phiT table
+    ! Fill the enthalpy-temperature table
     do i = 1,phiT_table_n_points                 
 
-       temp_table(i) = temp_table(i-1) + phiT_table_dT  ! Update temperature 
+       ! Update temperature
+       temp_table(i) = temp_table(i-1) + phiT_table_dT 
        
-       ! Solid-liquid phase transfer in temp-enth table 
+       ! Check if the updated temperature falls above the melting point
        if (temp_table(i).ge.melt_point) then
-          
-          if (isolid) then  ! First data point above melting temperature  
 
-             isolid = .false.  ! 
-             imelt = i         ! Index of melt data point 
-             temp_table(i) = melt_point             ! Exactly pin-point melt temperature in table 
-             phiT_table_dT = melt_point - temp_table(i-1)   ! 
+          ! Operations to perform for the first data point above melting temperature  
+          if (isolid) then  
+
+             isolid = .false.   
+             imelt = i          
+             temp_table(i) = melt_point 
+             phiT_table_dT = melt_point - temp_table(i-1)
              rhocp_im1 = rhocp_i 
              call get_rho(temp_table(i),rho) 
              call get_Cp(temp_table(i),Cp)  
-             rhocp_i = rho*Cp ! product of density and heat capacity at temperature  
+             rhocp_i = rho*Cp  
              enth_table(i) = enth_table(i-1) + (rhocp_i+rhocp_im1)*phiT_table_dT/2_amrex_real   ! Enthalpy at melt onset 
              enth_at_melt = enth_table(i) 
              phiT_table_dT = (phiT_table_max_T - melt_point)/(phiT_table_n_points-1-i)  ! New phiT_table_dT to match phiT_table_max_T !
              
           end if
-          
-          if(imelt.eq.i-1) then ! solid-liquid phase transfer jump 
 
-             temp_table(i) = melt_point ! 
+          ! Solid-liquid phase transfer jump
+          if(imelt.eq.i-1) then
+
+             temp_table(i) = melt_point
              enth_table(i) = enth_table(i-1) + 1E6*enth_fus*rho_melt/m_A ! [J/m3] = 1E6*[kJ/mol]*[kg/m3]/[g/mol]
-             
+
+          ! Compute enthalpy for all the state points above the melting point
           elseif(imelt.ne.i) then
              
-             rhocp_im1 = rhocp_i ! Product of density and heat capacity at previous temperature 
+             rhocp_im1 = rhocp_i 
              call get_rho(temp_table(i),rho) 
              call get_Cp(temp_table(i),Cp)  
-             rhocp_i = rho*Cp ! product of density and heat capacity at temperature       
+             rhocp_i = rho*Cp       
              enth_table(i) = enth_table(i-1) + (rhocp_i+rhocp_im1)*phiT_table_dT/2_amrex_real  ! Trapezoidal integration
              
           end if
           
        else
-          
-          rhocp_im1 = rhocp_i            ! Product of density and heat capacity at previous temperature 
+
+          ! Compute enthalpy for all the state points below the melting point
+          rhocp_im1 = rhocp_i 
           call get_rho(temp_table(i),rho) 
           call get_Cp(temp_table(i),Cp)  
-          rhocp_i = rho*Cp ! product of density and heat capacity at temperature 
+          rhocp_i = rho*Cp 
           enth_table(i) = enth_table(i-1) + (rhocp_i+rhocp_im1)*phiT_table_dT/2_amrex_real  ! Trapezoidal integration
           
        end if
@@ -243,16 +248,16 @@ contains
     end do
 		
     ! Output employed material properties to file
-    open (2, file = 'Employed_'//TRIM(material)//'_properties.txt', status = 'unknown') 
-     write(2,*) 'Material properties employed' 
-     write(2,*) 'Temperature, Cp [J/kgK], rho [kg/m3], k [W/mk]' 
-     do i = 0,phiT_table_n_points
-        call get_Cp(temp_table(i),Cp) 
-        call get_ktherm(temp_table(i),ktherm) 
-        call get_rho(temp_table(i),rho)
-        write(2,*) temp_table(i), Cp, rho, ktherm, enth_table(i)
-     end do
-     close(2) 
+    open (2, file = 'Employed_'//TRIM(material)//'_properties.txt', status = 'unknown')
+    write(2,*) 'Material properties employed' 
+    write(2,*) 'Temperature[K], Cp [J/kgK], rho [kg/m^3], k [W/mk], enthalpy [J/m^3]' 
+    do i = 0,phiT_table_n_points
+       call get_Cp(temp_table(i),Cp) 
+       call get_ktherm(temp_table(i),ktherm) 
+       call get_rho(temp_table(i),rho)
+       write(2,*) temp_table(i), Cp, rho, ktherm, enth_table(i)
+    end do
+    close(2) 
 
   end subroutine init_mat_prop
   
@@ -265,35 +270,33 @@ contains
                       t_lo , t_hi , temp) 
 
     ! Input and output variables
-    integer         , intent(in   ) :: lo(3), hi(3)
-    integer         , intent(in   ) :: uo_lo(3), uo_hi(3)
-    integer         , intent(in   ) :: t_lo(3), t_hi(3)
-    real(amrex_real), intent(in   ) :: phi (uo_lo(1):uo_hi(1),uo_lo(2):uo_hi(2),uo_lo(3):uo_hi(3))  ! Enthalpy     [j/m3]
-    real(amrex_real), intent(  out) :: temp(t_lo(1):t_hi(1),t_lo(2):t_hi(2),t_lo(3):t_hi(3))  ! Temperature  [K]
+    integer, intent(in) :: lo(3), hi(3)
+    integer, intent(in) :: uo_lo(3), uo_hi(3)
+    integer, intent(in) :: t_lo(3), t_hi(3)
+    real(amrex_real), intent(in) :: phi (uo_lo(1):uo_hi(1),uo_lo(2):uo_hi(2),uo_lo(3):uo_hi(3))
+    real(amrex_real), intent(out) :: temp(t_lo(1):t_hi(1),t_lo(2):t_hi(2),t_lo(3):t_hi(3))
 
     ! Local variables
-    real(amrex_real) :: rho, Cp  ! Mass density [kg/m3], Specific heat capacity [j/kgK]
-    integer :: i,j,k
     integer :: e_ind 
-    real(amrex_real) :: phi_melt_onset, phi_melt_finish, enth_fus
-    real(amrex_real) :: alph 
-    ! Tabulated temp as function of enthalpy 
-    ! Enthalpy is phi = int_0^T rho(T')*Cp(T') dT' 	
-    
-	
-    ! Enthalpy given as integral phi = int_0^T rho(T')*Cp(T') dT'. When initializing problem, perform integral and create table 
-    ! Get temp interpolates temp(phi) for each point in the lo,hi box.    
+    integer :: i,j,k
+    real(amrex_real) :: int_coeff 
+     
+    ! Obtain the temperature from linear interpolation of the enthalpy-temperature tables
     do i = lo(1),hi(1)
        do j = lo(2),hi(2)
           do k = lo(3),hi(3)
-             !Linear interpolation of temperature by given enthalpy, from table 
+             
              do e_ind = 0,phiT_table_n_points 
                 if (phi(i,j,k) .le. enth_table(e_ind) ) exit 
              end do
+
              if (e_ind.eq.phiT_table_n_points) STOP 'Temperature table exceeded' 
-             alph = (phi(i,j,k)-enth_table(e_ind-1))/(enth_table(e_ind)-enth_table(e_ind-1))
-             temp(i,j,k) = temp_table(e_ind-1) + alph*(temp_table(e_ind)-temp_table(e_ind-1))
-             !print *, i, j, k, phi(i,j,k), temp(i,j,k)
+
+             int_coeff = (phi(i,j,k)-enth_table(e_ind-1))/ &
+                    (enth_table(e_ind)-enth_table(e_ind-1))
+             temp(i,j,k) = temp_table(e_ind-1) + &
+                           int_coeff*(temp_table(e_ind)-temp_table(e_ind-1))
+             
           end do
        end do
     end do
@@ -311,46 +314,55 @@ contains
     integer :: e_ind 
     real(amrex_real), intent(in) :: temp
     real(amrex_real), intent(out) :: enth	
-    real(amrex_real) :: alph 
+    real(amrex_real) :: int_coeff 
 
-    !Linear interpolation of temperature by given enthalpy, from table
+    ! Obtain the enthalpy from linear interpolation of the enthalpy-temperature tables
     do e_ind = 0,phiT_table_n_points 
        if (temp .le. temp_table(e_ind) ) exit 
     end do
-    if (e_ind.eq.phiT_table_n_points) STOP 'Temperature table exceeded' 
-    alph = (temp-temp_table(e_ind-1))/(temp_table(e_ind)-temp_table(e_ind-1))
-    enth = enth_table(e_ind-1) + alph*(enth_table(e_ind)-enth_table(e_ind-1))
+    
+    if (e_ind.eq.phiT_table_n_points) STOP 'Temperature table exceeded'
+    
+    int_coeff = (temp-temp_table(e_ind-1))/(temp_table(e_ind)-temp_table(e_ind-1))
+    enth = enth_table(e_ind-1) + int_coeff*(enth_table(e_ind)-enth_table(e_ind-1))
      
   end subroutine get_enthalpy
 	
 	
 	
-	
-  ! subroutine to find maximum diffusivity for variable timestep 
+  ! ------------------------------------------------------------------
+  ! Subroutine used to obtain the maximum diffusivity. Could be
+  ! computed whenever the temperature is mapped on to the enthalpy.
+  ! ------------------------------------------------------------------  
   ! If used, called at timestep_mod, subr. increment after second get_temp. diff has to be reset before call to timestep (main)
-  subroutine get_maxdiffus(lo, hi, 		 &
-       t_lo , t_hi , temp) 
-    integer         , intent(in) :: lo(3), hi(3)				
-    integer         , intent(in) :: t_lo(3), t_hi(3)			
-    real(amrex_real), intent(in) :: temp(t_lo(1):t_hi(1),t_lo(2):t_hi(2),t_lo(3):t_hi(3))  ! Temperature  [K] 
-    integer :: i,j,k 			
-    real(amrex_real) :: rho, Cp, ktherm, diffus 
-    
-    do   i = lo(1),hi(1)
-       do  j = lo(2),hi(2) 
-  	  do k = lo(3),hi(3)
-            call get_ktherm(temp(i,j,k),ktherm)
-            call get_rho(temp(i,j,k),rho) 
-            call get_Cp(temp(i,j,k),Cp) 
-            diffus = ktherm/(rho*Cp) 
-            if (diffus.gt.max_diffus) max_diffus = diffus 
-         end do
-      end do
-   end do
+  subroutine get_maxdiffus(lo, hi, &
+                           t_lo , t_hi , temp) ! Revert the order oof t_lo, t_hi and temp
+
+    ! Input and output variables
+    integer, intent(in) :: lo(3), hi(3)				
+    integer, intent(in) :: t_lo(3), t_hi(3)			
+    real(amrex_real), intent(in) :: temp(t_lo(1):t_hi(1),t_lo(2):t_hi(2),t_lo(3):t_hi(3))
+
+    ! Local variables
+    integer :: i,j,k
+    real(amrex_real) :: Cp
+    real(amrex_real) :: diffus
+    real(amrex_real) :: ktherm
+    real(amrex_real) :: rho 
+
+    do i = lo(1),hi(1)
+       do j = lo(2),hi(2)
+          do k = lo(3),hi(3)
+             call get_ktherm(temp(i,j,k),ktherm)
+             call get_rho(temp(i,j,k),rho) 
+             call get_Cp(temp(i,j,k),Cp) 
+             diffus = ktherm/(rho*Cp) 
+             if (diffus.gt.max_diffus) max_diffus = diffus 
+          end do
+       end do
+    end do
 
  end subroutine get_maxdiffus
-
-	
 
 
 end module material_properties_module 
